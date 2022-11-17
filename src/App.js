d347502745a217542f0a5d6ca5bdef9a92b9d7a2
ROBERT-Gimenez <robertgim37@gmail.com>
2022-11-16 21:46:47 -0300

    header and footer added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Login from './components/Login';
 import {Routes , Route} from 'react-router-dom'
 import Listado from './components/Listado';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-    <Routes>
+      <Header/>
+      <Routes>
       <Route path="/" element={<Login/>}></Route>
       <Route path="/listado" element={<Listado/>}></Route>
     </Routes>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <nav>
+            <ul>
+                <li><a href='http://instagram.com' rel="noopener noreferrer">IG</a></li>
+            </ul>
+        </nav>
+        <p>Copyright footer example</p>
+    </footer>
+  )
+}

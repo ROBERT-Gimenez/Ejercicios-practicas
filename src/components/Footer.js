c8cb1c4185eaec83a718d394e95132830c803fe8
ROBERT-Gimenez <robertgim37@gmail.com>
2022-11-17 21:21:54 -0300

    api data consumption

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
    
   return (
-    <footer className='flex-md-column justify-content-center navbar navbar-expand-lg navbar-dark bg-dark position-absolute top-100 start-50 translate-middle' style={{width:"100%"}}>
+    <footer className='flex-md-column justify-content-center navbar navbar-expand-lg navbar-dark bg-dark' style={{width:"100%"}}>
         <nav className='d-flex align-items-center' >
             <ul className='list-unstyled'>
                 <li><a className='navbar-brand' href='http://instagram.com' rel="noopener noreferrer">IG</a></li>
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,10 +1,11 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import { useNavigate , Link , Navigate } from 'react-router-dom'; //en react v6 para el redireccionamiento se usa el navigate y useNavigate
-
+import axios from 'axios';
 
 function Listado() {
     const token = localStorage.getItem('token');
     const history = useNavigate();
+    const [moviesList , setMovies] = useState([]);
     
   /*Una manera de redireccionar 
     useEffect(() => {
@@ -13,23 +14,36 @@ function Listado() {
         }
     } , []) */
 
+
+    useEffect(() => {
+        const endpoint = 'https://api.themoviedb.org/3/discover/movie?api_key=d6c22a610db913393d63611f4566f0a1&language=en-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
+        axios.get(endpoint)
+        .then(response => {
+            const apiData = response.data;
+            setMovies(apiData.results)
+            console.log(moviesList)
+        })
+    } , [])
+
   return (
     <>
     {!token && <Navigate to="/" /> }
     <div className='row'>
-        <div className='col-3' style={{border:'1px solid red'}}>
-        <div className="card" style={{width: "18rem"}}>
-  <img href="..." className="card-img-top" />
-  <div className="card-body">
-    <h5 className="card-title">Movie title</h5>
-    <p className="card-text">Reviw movie Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quae id quis architecto obcaecati vel amet iste tempora, similique non enim ex, optio necessitatibus ullam expedita nihil odit totam quod.
-    Commodi perferendis labore dignissimos deserunt, dolores, quod eum aliquid distinctio eveniet mollitia nihil iure quas beatae quia tenetur possimus ex? Quam a dicta at, commodi incidunt quia dolorem facere reprehenderit. </p>
-    <Link to="/" className="btn btn-primary">View Detail</Link>
-  </div>
-</div>
-        </div>
+    {moviesList.map((movie,inx) => {
+        return (
+            <div key={inx} className='col-3' style={{border:'1px solid red'}}>
+            <div className="card" style={{width: "18rem"}}>
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title">{movie.title}</h5>
+                    <p className="card-text">{movie.overview.substring(0 , 100)}</p>
+                <Link to="/" className="btn btn-primary">View Detail</Link>
+                </div>
+            </div>
+            </div>
 
-
+                )
+    })}
     </div>
     </>
   )
